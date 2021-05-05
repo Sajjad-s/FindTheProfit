59c307345011de49aa92f5236851343dc096d560
Sajjad-s <darma.sajjad@gmail.com>
2021-05-05 20:23:40 +0430

    after the debugging

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         int[] priceArray = main.getInput();
-        System.out.println( calculateMaxProfit(priceArray));
+        int result = calculateMaxProfit(priceArray);
+        System.out.println("The most profit from this periods is: " + result);
     }
-
     private int[] getInput() {
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number of days: ");
         int daysNumber = scanner.nextInt();
         int[] priceHistory = new int[daysNumber];
+        System.out.println("Enter the each day price: ");
         for (int i = 0; i < daysNumber; i++) {
+            System.out.println("the Day number " + (i + 1) +" price is: ");
             priceHistory[i] = scanner.nextInt();
         }
         return priceHistory;
     }
-
-
     public static int calculateMaxProfit(int[] inputArray) {
         int lowestPrice = inputArray[0];
         int maxProfit = Integer.MIN_VALUE;
